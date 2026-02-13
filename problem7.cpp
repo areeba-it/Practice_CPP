a5352dc1a23c48afaf89bf1d41163c179ec9ea6b
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-02-13 23:25:12 +0500

    Secound number is sequare of first number

---
+++ problem7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a number";
+    cin>>a;
+    cout<<"Enter secound number";
+    cin>>b;
+    if(a*a==b)
+    cout<<"2nd number is sequare of first number";
+    return 0;
+}
