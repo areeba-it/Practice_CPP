f61d210e54e767faca1510aa5c0525b7035eefdd
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-02-14 22:10:16 +0500

    sum

---
+++ problem9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b; int sum=0;
+    cout<<"Enter a number";
+    cin>>a;
+    cout<<"Emter a number";
+    cin>>b;
+   sum=sum+a;
+   cout<<"sum is";
+   return 0;
+}
