466f980f675b71970ad645efb481a929e854a08f
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-02-24 23:11:14 +0500

    while loop

---
+++ program28.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int index=0;
+    while(index<35){
+        cout<<"you are at index number"<<index<<endl;
+        index++;
+    }
+    return 0;
+}
