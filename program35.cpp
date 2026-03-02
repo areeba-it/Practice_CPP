844b482171380a405247ac52d860e7b303764b23
Areeba Iqbal <areebaiqbalit@gmail.com>
2026-03-02 23:20:34 +0500

    Sum of Array

---
+++ program35.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5], sum = 0;
+
+    cout << "Enter 5 numbers:\n";
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
